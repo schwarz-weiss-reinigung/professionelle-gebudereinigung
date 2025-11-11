cb75091a8fc4f10964bf3a2d8adafc129e21c13c
schwarz-weiss-reinigung <karl.mirrorsinnovation@gmail.com>
2025-11-11 13:55:25 +0500

    Update index.php

--- index.php
+++ index.php
@@ -90,6 +90,8 @@ $contract_points = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-site-verification" content="3mzUiSIoLmrq_m8ZCPRRbJlnHo78RZncHEqYA3HMC7g" />
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <meta name="googlebot" content="index, follow">
     <meta name="description" content="<?php echo htmlspecialchars($meta_description); ?>">
     <meta name="keywords" content="<?php echo htmlspecialchars($meta_keywords); ?>">
     <meta name="author" content="<?php echo htmlspecialchars($author); ?>">
